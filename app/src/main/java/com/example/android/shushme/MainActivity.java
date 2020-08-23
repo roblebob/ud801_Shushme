e16ff33a6ac83c4221d0c0525c0ea6fbad05ad11
roblebob <robert.koppisch@gmail.com>
2020-08-24 00:37:07 +0200

    ...

--- app/src/main/java/com/example/android/shushme/MainActivity.java
+++ app/src/main/java/com/example/android/shushme/MainActivity.java
@@ -18,12 +18,14 @@ package com.example.android.shushme;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
